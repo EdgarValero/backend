30ccec17269431756d1f38ed7693ef435f109fa4
Edgar Valero <edgaralexandervb@gmail.com>
2021-05-24 11:14:46 -0400

    Mensaje de inicio del servidor cambiado.

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ app.get('/', (req, res) => {
 	res.send('Hola Mundo');
 });
 
-app.listen(4000, () => console.log('Servidor en el puerto 3000'));
+app.listen(4000, () => console.log('Servidor en el puerto 4000'));
