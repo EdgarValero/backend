ea642583c8c87c8324b99b38eeb9fa5cea2ba227
Edgar Valero <edgaralexandervb@gmail.com>
2021-06-28 15:21:25 -0400

    Practica sobre programacion asincrona para realizar la e-actividad 3.1 Programacion Asincrona.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Rutas
-app.use('/api/structures', require('./routes/structures.js'));
+app.use('/api/structures', require('./routes/structures.routes.js'));
+app.use('/api/cars', require('./routes/tasks.routes.js'));
 
 app.listen(4000, () => console.log('Servidor en el puerto 4000'));
